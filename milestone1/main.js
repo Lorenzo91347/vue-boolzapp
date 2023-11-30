32daa3f29c550613409d323b45d707989d0f8321
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-30 16:42:20 +0100

    feat:v-if

--- milestone1/main.js
+++ milestone1/main.js
@@ -15,17 +15,17 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            Status: 'received'
                         }
                     ],
                 },
@@ -37,17 +37,17 @@ createApp({
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            Status: 'received'
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            Status: 'sent'
                         }
                     ],
                 },
@@ -59,17 +59,17 @@ createApp({
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
-                            status: 'received'
+                            Status: 'received'
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
-                            status: 'received'
+                            Status: 'received'
                         }
                     ],
                 },
@@ -81,12 +81,12 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            Status: 'received'
                         }
                     ],
                 },
@@ -98,12 +98,12 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
-                            status: 'received'
+                            Status: 'received'
                         }
                     ],
                 },
@@ -115,17 +115,17 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
-                            status: 'received'
+                            Status: 'received'
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            Status: 'sent'
                         }
                     ],
                 },
@@ -137,12 +137,12 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                            status: 'received'
+                            Status: 'received'
                         }
                     ],
                 },
@@ -154,33 +154,31 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                            status: 'received'
+                            Status: 'received'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
+                            Status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
-                            status: 'received'
+                            Status: 'received'
                         }
                     ],
                 }
             ],
-            index: 0
+            index: 0,
+
         }
     },
     methods: {
-        getDate(arr) {
-            const now = arr;
 
-            const hoursAndMinutes = now.getHours() + ':' + now.getMinutes();
-            console.log(hoursAndMinutes);
-        },
+    },
 
-
-    }
 }).mount('#app');
+
+
+
 
